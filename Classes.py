e1796d612885c8386ab0ba50606afde8f434d8ca
Uzma <uzma.aliza@gmail.com>
2018-08-30 15:57:16 -0500

    Writes out new csv each time a word is saved.

--- Classes.py
+++ Classes.py
@@ -14,6 +14,7 @@ class Dictionary:
     def __init__(self):
         data = pd.read_csv("Dictionary.csv", header=0)
         self.df = pd.DataFrame(data)
+        print(self.df)
 
     def retrieveword(self, word):
         row = self.df.loc[self.df['Word'] == word]
@@ -24,16 +25,18 @@ class Dictionary:
             print("The word is: ", word.wordName[0])
             print("The meaning of the word is: ", word.definition[0])
             print("And it's part of speech value is: ", word.partOfSpeech[0])
+        else:
+            print("Error")
 
     def saveWord(self, word, partOfSpeech, definition):
         row = pd.DataFrame({'Word': [word], 'PartOfSpeech': [partOfSpeech], 'Definition': [definition]})
-        # print(row)
         self.df = self.df.append(row)
         print(self.df)
-
-
+        with open("Dictionary.csv", 'w') as f:
+            self.df.to_csv(f, header=True)
 
     # def addWord(self):
+
 
 
 
@@ -41,3 +44,6 @@ class Dictionary:
 dict = Dictionary()
 dict.retrieveword("Chicken")
 dict.saveWord("Monkey", "Noun", "An animal")
+
+dict.retrieveword("Monkey")
+dict.saveWord("Book","Noun", "Something to read")
