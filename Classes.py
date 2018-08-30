c6c226a1f0fd020612d44bc4eb373037565a5e3c
Uzma <uzma.aliza@gmail.com>
2018-08-30 15:24:46 -0500

    Program can retrieve and save words

--- Classes.py
+++ Classes.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 
 class Word:
@@ -8,13 +9,35 @@ class Word:
         self.definition = definition
 
 class Dictionary:
+    df = None
+
     def __init__(self):
         data = pd.read_csv("Dictionary.csv", header=0)
-        df = pd.DataFrame(data)
+        self.df = pd.DataFrame(data)
 
-    def addWord(self, word):
+    def retrieveword(self, word):
+        row = self.df.loc[self.df['Word'] == word]
+        # print(row)
+        if row is not None:
+            word = Word(row["Word"], row["PartOfSpeech"], row["Definition"])
+            print()
+            print("The word is: ", word.wordName[0])
+            print("The meaning of the word is: ", word.definition[0])
+            print("And it's part of speech value is: ", word.partOfSpeech[0])
+
+    def saveWord(self, word, partOfSpeech, definition):
+        row = pd.DataFrame({'Word': [word], 'PartOfSpeech': [partOfSpeech], 'Definition': [definition]})
+        # print(row)
+        self.df = self.df.append(row)
+        print(self.df)
 
 
-    def retrieveWord(self, word):
 
-    def saveWord(self, word):
+    # def addWord(self):
+
+
+
+
+dict = Dictionary()
+dict.retrieveword("Chicken")
+dict.saveWord("Monkey", "Noun", "An animal")
