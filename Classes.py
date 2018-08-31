36165ffa39afff84f4336f304fbee249c93f1450
Uzma <uzma.aliza@gmail.com>
2018-08-30 23:14:23 -0500

    Refined

--- Classes.py
+++ Classes.py
@@ -1,15 +1,12 @@
 
 import pandas as pd
 
-class Word:
-    def __init__(self, wordName, partOfSpeech, definition):
-        # population array
-        self.wordName = wordName
-        self.partOfSpeech = partOfSpeech
-        self.definition = definition
 
 class Dictionary:
     df = None
+    wordName = None
+    partOfSpeech = None
+    definition = None
 
     def __init__(self):
         data = pd.read_csv("Dictionary.csv", header=0)
@@ -18,32 +15,40 @@ class Dictionary:
 
     def retrieveword(self, word):
         row = self.df.loc[self.df['Word'] == word]
-        # print(row)
-        if row is not None:
-            word = Word(row["Word"], row["PartOfSpeech"], row["Definition"])
+        if row.empty is False:
+            wordName = row['Word'].to_string(index=False)
+            definition = row["Definition"].to_string(index=False)
+            partOfSpeech = row["PartOfSpeech"].to_string(index=False)
             print()
-            print("The word is: ", word.wordName[0])
-            print("The meaning of the word is: ", word.definition[0])
-            print("And it's part of speech value is: ", word.partOfSpeech[0])
+            print("The word is: ", wordName)
+            print("The meaning of the word is: ", definition)
+            print("And it's part of speech value is: ", partOfSpeech)
         else:
-            print("Error")
+            print()
+            print("Error: Word does not exist")
 
     def saveWord(self, word, partOfSpeech, definition):
-        row = pd.DataFrame({'Word': [word], 'PartOfSpeech': [partOfSpeech], 'Definition': [definition]})
-        self.df = self.df.append(row)
-        print(self.df)
-        with open("Dictionary.csv", 'w') as f:
-            self.df.to_csv(f, header=True)
-
-    # def addWord(self):
-
+        row = self.df.loc[self.df['Word'] == word]
+        if row.empty is True:
+            row = pd.DataFrame({'Word': [word], 'PartOfSpeech': [partOfSpeech], 'Definition': [definition]})
+            self.df = self.df.append(row, sort = False)
+            print(self.df)
+            with open("Dictionary.csv", 'w') as f:
+                self.df.to_csv(f, header=True, index=False)
+        else:
+            print()
+            print("Error: Word already exists")
 
 
 
 
 dict = Dictionary()
 dict.retrieveword("Chicken")
-dict.saveWord("Monkey", "Noun", "An animal")
+# dict.saveWord("Monkey", "Noun", "An animal")
 
 dict.retrieveword("Monkey")
+# dict.saveWord("Book","Noun", "Something to read")
+dict.retrieveword("Book")
+
 dict.saveWord("Book","Noun", "Something to read")
+dict.retrieveword("Poop")
